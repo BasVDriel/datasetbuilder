f0a429271a8fea8458a74af01d4151d0cff6a38c
= <=>
2025-04-23 16:12:43 +0200

    tiling_test works again, I can now find all tiles that have trees on them!

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ ahn_tile_fn = "ahn_subtiles.zip"
 ahn_subtile_fn = "ahn_tiles.zip"
 utrecht_trees_fn = "utrecht_trees.gpkg"
 
-ahn_subtile_index = os.path.join(source_dir, ahn_subtile_fn)
-ahn_tile_index = os.path.join(source_dir, ahn_tile_fn)
-utrecht_boomkaart = os.path.join(source_dir, utrecht_trees_fn)
+ahn_subtile_path = os.path.join(source_dir, ahn_subtile_fn)
+ahn_tile_path = os.path.join(source_dir, ahn_tile_fn)
+utrecht_trees_path = os.path.join(source_dir, utrecht_trees_fn)
 
 utrecht_trees_url = "https://arcgis.com/sharing/rest/content/items/7e2404cf7fba4bb087935f9cdb51f053/data"
 ahn_subtile_url = "https://static.fwrite.org/2023/01/AHN_subunits_GeoTiles.zip"
@@ -16,12 +16,14 @@ ahn_tile_url = "https://static.fwrite.org/2023/01/AHN_AHN_GeoTiles.zip"
 orthomosaic_wmts_url = "https://service.pdok.nl/hwh/luchtfotorgb/wmts/v1_0?request=GetCapabilities&service=wmts"
 
 def get_sources():
+    """"
+    Downloads some key sources like tile indices that are required for tile downloading
+    """
     from utils.tiles import download_url
     print("Downloading sources from source urls")
     download_url(ahn_subtile_url, source_dir, filename=ahn_subtile_fn)
     download_url(ahn_tile_url, source_dir, filename=ahn_tile_fn)
     download_url(utrecht_trees_url, source_dir, filename=utrecht_trees_fn)
-
 
 def wmts_test():
     from utils.wtms import WMTSBuilder, WMTSMap
@@ -39,26 +41,32 @@ def wmts_test():
     t2 = time.time()
     print(t2-t1)
 
-def ahn_test():
+def tiling_test():
+    import pandas as pd
     import geopandas as gpd
     import matplotlib.pyplot as plt
-    from utils.tiles import TileDownloader
 
-    # Paths
-    ahn_tiles_path = "zip://" + ahn_tile_index
-    geojson_path = utrecht_boomkaart
-    gdf_polygons = gpd.read_file(ahn_tiles_path)
-    gdf_points = gpd.read_file(geojson_path)
-    em_polygons = gpd.read_file(elevation_model_index)
+    ahn_tiles_path = "zip://" + ahn_tile_path
+    tile_polygons_gdf = gpd.read_file(ahn_tiles_path)
+    subtile_polygons_gdf = gpd.read_file(ahn_subtile_path)
 
+    # merge the two layers in the geopackage of the utrecht trees
+    utrecht_trees_bos_gdf = gpd.read_file(utrecht_trees_path, layer=0)
+    utrecht_trees_weg_gdf = gpd.read_file(utrecht_trees_path, layer=1)
+    utrecht_trees_gdf = pd.concat([utrecht_trees_bos_gdf, utrecht_trees_weg_gdf], axis=0)
+    
     # Joint the points and polygons that contain a tree from the utrecht dataset
-    joined = gpd.sjoin(gdf_polygons, gdf_points, how="inner", predicate="contains")
-    result_polygons = gdf_polygons[gdf_polygons.index.isin(joined.index)]
+    joined = gpd.sjoin(tile_polygons_gdf, utrecht_trees_gdf, how="inner", predicate="contains")
+    result_tile_polygons = tile_polygons_gdf[tile_polygons_gdf.index.isin(joined.index)]
 
-    print(result_polygons.columns)
-    print(result_polygons["GT_AHNSUB"])
-    print(result_polygons["GT_AHN"])
+    # Also find the subtiles that contain a tree
+    joined_subtiles = gpd.sjoin(subtile_polygons_gdf, utrecht_trees_gdf, how="inner", predicate="contains")
+    result_subtile_polygons = subtile_polygons_gdf[subtile_polygons_gdf.index.isin(joined_subtiles.index)]
 
+    # aplot the result polygons for tiles and subtiles
+    ax = result_tile_polygons.plot(color='blue', alpha=0.5, edgecolor='black', label="Tiles")
+    result_subtile_polygons.plot(ax=ax, color='green', alpha=0.5, edgecolor='black', label="Subtiles")
+    plt.show()
 
 
     
